a083d4b13d034f520dc8fd97c1cd60cccd93eb76
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-08-31 15:24:04 -0500

    using ts now and added interface for prop API

---
+++ src/components/Toast/Toast.tsx
@@ -0,0 +1,49 @@
+import React, { Dispatch, SetStateAction } from "react";
+import {
+  AlertOctagon,
+  AlertTriangle,
+  CheckCircle,
+  Info,
+  X,
+} from "react-feather";
+
+import VisuallyHidden from "../VisuallyHidden";
+import { VariantType } from "../ToastPlayground/ToastPlayground";
+
+import styles from "./Toast.module.css";
+
+const ICONS_BY_VARIANT = {
+  notice: Info,
+  warning: AlertTriangle,
+  success: CheckCircle,
+  error: AlertOctagon,
+};
+
+interface ToastProps {
+  variant: VariantType;
+  message: string;
+  setShowToast: Dispatch<SetStateAction<boolean>>;
+}
+
+function Toast({ variant, message, setShowToast }: ToastProps) {
+  const Icon = ICONS_BY_VARIANT[variant];
+  return (
+    <div className={`${styles.toast} ${styles[variant]}`}>
+      <div className={styles.iconContainer}>
+        <Icon size={24} />
+      </div>
+      <p className={styles.content}>{message}</p>
+      <button
+        className={styles.closeButton}
+        onClick={() => {
+          setShowToast(false);
+        }}
+      >
+        <X size={24} />
+        <VisuallyHidden>Dismiss message</VisuallyHidden>
+      </button>
+    </div>
+  );
+}
+
+export default Toast;
